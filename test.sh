f471738035dc8c14878578ec534a6c79090a6996
walts1986 <45971183+walts1986@users.noreply.github.com>
2020-10-27 16:05:43 -0400

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,9 @@
+cd /home/cznkfw/es5/sa8195p-qx-0-1_hlos_dev_qnx/apps/qnx_ap
+sleep 2
+pwd
+source setenv_64.sh --external /home/cznkfw/qnx700_es4/
+sleep 5
+cd /home/cznkfw/es5/sa8195p-qx-0-1_hlos_dev_qnx/apps/qnx_ap/AMSS/qaic/src/inferno
+sleep 2
+pwd
+./clean.sh
